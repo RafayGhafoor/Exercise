35d37e0ef942f9a50ba5597c7db630b4653ddc8d
RafayGhafoor <rafayghafoor@protonmail.com>
2017-10-04 03:56:02 -0400

    Updated functions

--- file.py
+++ file.py
@@ -10,32 +10,38 @@ def sumIt(lst):
     if not lst:
         return 0
     else:
-        return lst.pop(0) + sumIt(lst)
+        return lst[0] + sumIt(lst[1:])
 
 
 # Returns the number of elements in the list. (Ex. 4.2)
 def countIt(lst):
     if not lst:
         return 0
-    lst.pop(0)
-    return 1 + countIt(lst)
+    return 1 + countIt(lst[1:])
     
 
 # Finds maximum number in the list. (Ex. 4.3)
-max_num = 0
+# [1,2,3,1,3] | [2,3,1,3] | [3,1,3] | [1,3]
 def findMax(lst):
-    global max_num
-    if not lst:
+    if len(lst) == 2:
+        return lst[1] if lst[0] < lst[1]  else lst[0]
+    max_num = findMax(lst[1:])
+    if lst[0] < max_num:
         return max_num
-    lst_item = lst.pop(0)
-    if  max_num < lst_item:    
-        max_num = lst_item
-    return findMax(lst)
+    else:
+        return lst[0]
+
+
+def findLeast(lst):
+    if len(lst) == 2:
+        return lst[0] if lst[0] < lst[1] else lst[1]
+    z = findLeast(lst[1:])
+    if lst[0] > z:
+        return z
+    else:
+        return lst[0]
+
    
                 
 g = [int(i) for i in sys.argv[1].split(',')]
-h = g[:]
-i = g[:]
-length = countIt(g)
-summation = sumIt(h)
-print("The Sum is {0}, Max is {2} and the length of the list {1}".format(summation, length, findMax(i)))
+print("The Sum is {0}, Max is {2}, length of the list {1} and Least value is {3}".format(sumIt(g[:]), countIt(g[:]), findMax(g[:]), findLeast(g[:])))
