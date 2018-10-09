17e343c1eef4a6966018eab1bcf8efc2e844a24f
Rafay Ghafoor <rafayghafoor@pm.me>
2018-10-09 12:07:31 +0500

    Update draw_triangle.cpp

--- C++/Code/draw_triangle.cpp
+++ C++/Code/draw_triangle.cpp
@@ -1,38 +1,43 @@
-#include <iostream>
+C++ shell
+cpp.sh
+online C++ compiler
+about cpp.sh
 
-using namespace std;
+
+RunGet URL
+optionscompilationexecution
+==============Drawing Right Triangle===============
+Enter size: 4
+****
+ ***
+  **
+   *
+==============Drawing Left Triangle===============
+****
+***
+**
+*
+ 
+Exit code: 0 (normal program termination)
+C++ Shell, 2014-2015C++ shell
+cpp.sh
+online C++ compiler
+about cpp.sh
 
-void spaces(int N) {
-    for (int i=1; i<=N; i++)
-        cout << " ";
-}
-
-void stars(int N) {
-    for (int i=1; i<=N; i++)
-        cout << "*";
-}
-
-int main() {
-    cout << "==============Print Right Triangle===============\n";
-    cout << "Enter size: "; int size;
-    cin >> size;
-    int a = 0;
-    for (int i=size; i>=0; i--, a++) {
-        spaces(a); stars(i); cout << endl;
-    }
-    
-    cout << "==============Print Left Triangle===============\n";
-    cout << "Enter size: "; int size1;
-    cin >> size1;
-    int a1 = 0;
-    for (int i=size1; i>=0; i--, a1++) {
-        
-        stars(i); spaces(a1); cout << endl;
-    }
-    
-}
-    
-    
-    
-    
-
+
+RunGet URL
+optionscompilationexecution
+==============Drawing Right Triangle===============
+Enter size: 4
+****
+ ***
+  **
+   *
+==============Drawing Left Triangle===============
+****
+***
+**
+*
+ 
+Exit code: 0 (normal program termination)
+C++ Shell, 2014-2015
