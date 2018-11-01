be250f8949ddf29cdd60efd4876d84a6a39d5f5a
Rafay Ghafoor <rafayghafoor@pm.me>
2018-11-01 13:24:23 +0500

    Moving

--- C++/Code/Find - Replace Utility.cpp
+++ C++/Code/Find - Replace Utility.cpp
@@ -31,12 +31,19 @@ bool findSubstring(char sentence[], char sub[], int index, int &foundIndex) {
   return match;
 }
 
-// void replaceSubStr(char sentence[], char sub[], int index) {}
+void replaceSubStr(char sentence[], char sub[], int index) {
+  int sub_len = strlen(sub) + index;
+
+  for (int i = 0; index < sub_len; index++, i++)
+    sentence[index] = sub[i];
+}
 
 int main() {
-  char sen[30] = "Rafay Ghafoor Awan";
-  char sub[10] = "or";
+  char sen[30] = "Rafay Ghafoas Awan";
+  char sub[10] = "as";
   int fIndex = 0;
   if (findSubstring(sen, sub, fIndex, fIndex))
     cout << "Yes\n" << fIndex << "\n";
+  replaceSubStr(sen, "or", fIndex);
+  cout << sen << endl;
 }
