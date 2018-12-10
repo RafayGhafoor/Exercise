119e8db37eda814ff0a1d0b0ff203af394010f4a
Rafay Ghafoor <rafayghafoor@pm.me>
2018-12-11 00:45:08 +0500

    Update Lab Practice (9).cpp

--- C++/Code/Lab Practice (9).cpp
+++ C++/Code/Lab Practice (9).cpp
@@ -7,30 +7,35 @@ using namespace std;
 // integer as well as write it in a file named fact_out.txt in a new line.
 // Factorial should be found using a function named factorial.
 
-int calc_Factorial(int num) {
-  int total = 1;
-  for (int i = 2; i <= num; i++)
-    total = total * i;
-  return total;
+int calc_Factorial(int num)
+{
+    int total = 1;
+    for (int i = 2; i <= num; i++)
+        total = total * i;
+    return total;
 }
 
-int main() {
-  // Write Integers to file
-  ofstream file("fact_data.txt");
+int main()
+{
+    // Write Integers to file
+    ofstream file("fact_data.txt");
 
-  // Add integers from 2 to 20 in the file
-  for (int i = 2; i <= 10; i++)
-    file << i << endl;
+    // Add integers from 2 to 20 in the file
+    for (int i = 2; i <= 10; i++)
+        file << i << endl;
 
-  file.close();
+    file.close();
 
-  //   Read Integers from file
-  ifstream afile("fact_data.txt");
+    //   Read Integers from file
+    ifstream afile("fact_data.txt");
 
-  int num;
-  afile >> num;
-  while (!afile.eof()) {
-    cout << "Factorial of " << num << " is " << calc_Factorial(num) << endl;
+    ofstream bfile("fact_out.txt");
+    int num;
     afile >> num;
-  }
+    while (!afile.eof())
+    {
+        cout << "Factorial of " << num << " is " << calc_Factorial(num) << endl;
+        bfile << calc_Factorial(num) << endl;
+        afile >> num;
+    }
 }
