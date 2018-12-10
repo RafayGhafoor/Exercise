64dc4d6508fa3d7a7412aed9d484ca4cc305d345
Rafay Ghafoor <rafayghafoor@pm.me>
2018-12-11 00:17:17 +0500

    Update Lab Practice (7).cpp

--- C++/Code/Lab Practice (7).cpp
+++ C++/Code/Lab Practice (7).cpp
@@ -2,34 +2,36 @@
 #include <iostream>
 using namespace std;
 
-int main()
-{
-    char names[5][30];
-    int votes[5], total_votes = 0;
+int getMaxIndex(int arr[], int size) {
+  int maxIndex = 0;
+  for (int i = 0; i < size; i++)
 
-    for (int i = 0; i < 5; i++)
-    {
-        cout << "Please enter name for candidates no. " << i << " : ";
-        cin >> names[i];
-        cout << "Enter number of votes received by the candidate: ";
-        cin >> votes[i];
-        total_votes += votes[i];
-    }
+    if (arr[i] > arr[maxIndex])
+      maxIndex = i;
 
-    // \t means 4 spaces
-    cout << "Candidate\tVotes Received\t% of Total Votes" << endl;
-    
-    // Curious about How Formatting is done?
-    // Read 2nd answer with 32 UPVOTES: 
-    // https://stackoverflow.com/questions/15884284/c-printing-spaces-or-tabs-given-a-user-input-integer
-    for (int i = 0; i < 5; i++)
-    {
-        cout << names[i] << string(strlen(names[i]), ' ')
-             << string(strlen("votes received\t"), ' ') << votes[i]
-             << string(strlen("% of total votes"), ' ')
-             << (votes[i] * 1.0) / total_votes << endl;
-    }
+  return maxIndex;
+}
 
-    // cout << "The Winner of the Election is: "
-    //      << "2";
+int main() {
+  char names[5][30];
+  int votes[5], total_votes = 0;
+
+  for (int i = 0; i < 5; i++) {
+    cout << "Please enter name for candidates no. " << i << " : ";
+    cin >> names[i];
+    cout << "Enter number of votes received by the candidate: ";
+    cin >> votes[i];
+    total_votes += votes[i];
+  }
+
+  // \t means 4 spaces
+  cout << "Candidate\tVotes Received\t% of Total Votes" << endl;
+  for (int i = 0; i < 5; i++) {
+    cout << names[i] << string(strlen(names[i]), ' ')
+         << string(strlen("votes received\t"), ' ') << votes[i]
+         << string(strlen("% of total votes"), ' ')
+         << (votes[i] * 100.0) / total_votes << endl;
+  }
+
+  cout << "The Winner of the Election is: " << names[getMaxIndex(votes, 5)];
 }
