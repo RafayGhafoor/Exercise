48893254360ebc270b9a90cc513e55ba5970e191
Rafay Ghafoor <rafayghafoor@pm.me>
2018-12-10 23:05:49 +0500

    Update Lab Practice.cpp

--- C++/Code/Lab Practice.cpp
+++ C++/Code/Lab Practice.cpp
@@ -6,7 +6,7 @@ using namespace std;
 void initMatrix(int **matrix, int rows, int columns) {
   // Initialize the given matrix entries to 0
   for (int i = 0; i < rows; i++) {
-    matrix[i] = new int[rows];
+    matrix[i] = new int[columns];
     
     for (int j = 0; j < columns; j++)
       matrix[i][j] = 0;
