5baae4b8c379350cbe3bbbadb92d923edcac7fb8
Rafay Ghafoor <rafayghafoor@pm.me>
2018-12-16 16:13:49 +0000

    Update Mid2Q3.cpp

--- C++/Code/Mid2Q3.cpp
+++ C++/Code/Mid2Q3.cpp
@@ -5,8 +5,8 @@ int main() {
   int arr[10] = {77, 88, 11, 3, 1, 5, 6, 30, 23, 0};
   int maxDiff = -10000;
   int k = 2;
-  for (int i = 0; i < 9; i++)
-    for (int j = i + 1; j < 9; j++) {
+  for (int i = 0; i < 10; i++)
+    for (int j = 0; j < 10; j++) {
       if (j - i <= k && arr[j] - arr[i] > maxDiff) {
         maxDiff = arr[j] - arr[i];
       }
