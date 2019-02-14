e0caf471e53019ddee0809589c29b98917ea6012
Rafay Ghafoor <rafayghafoor@pm.me>
2019-02-14 11:50:07 +0500

    Updating

--- C++/Semester 2/Class Work/Class-04 [String Implementation].cpp
+++ C++/Semester 2/Class Work/Class-04 [String Implementation].cpp
@@ -3,58 +3,63 @@
 
 using namespace std;
 
-class String {
+class String
+{
 
-private:
-  char str[256];
+  private:
+    char str[256];
 
-public:
-  // Constructor 1 {Default Constructor}
-  String() {
-    str[0] = 0; // initialize the first index of str to null character
-  }
+  public:
+    // Constructor 1 {Default Constructor}
+    String()
+    {
+        str[0] = 0; // initialize the first index of str to null character
+    }
 
-  // Constructor 2
-  String(char input[]) { strcpy(str, input); }
+    // Constructor 2
+    String(char input[]) { strcpy(str, input); }
 
-  String concat(String const &obj) {
-    String ans = obj;
+    String concat(String const &obj)
+    {
+        String ans = obj;
 
-    int size = strlen(str), i = size, j = 0;
+        int size = strlen(str), i = size, j = 0;
 
-    for (; i < size + strlen(ans.str); i++, j++)
-      ans.str[size + i] = str[j];
+        for (; i < size + strlen(ans.str); i++, j++)
+            ans.str[size + i] = str[j];
 
-    ans.str[i] = 0;
+        ans.str[i] = 0;
 
-    return ans;
-  }
+        return ans;
+    }
 
-  const int length() {
-    int len = 0;
+    const int length()
+    {
+        int len = 0;
 
-    for (; str[len] != 0; len++)
-      ;
-    return len;
-  }
+        for (; str[len] != 0; len++)
+            ;
+        return len;
+    }
 
-  void print() { cout << str << endl; }
+    void print() { cout << str << endl; }
 };
 
 // A null terminated string (Constructor 1) [No argument specified]
 //
-int main() {
-  char abc[] = "Hello!", test[] = "abc";
-  String S(test), x, t(abc);
+int main()
+{
+    char abc[] = "Hello!", test[] = "abc";
+    String S(test), x, t(abc);
 
-  cout << "Length of t is: " << t.length() << endl;
+    cout << "Length of t is: " << t.length() << endl;
 
-  x = S.concat(t); // Concatenates the content of t at the end of s and the
-                   // resultant is copied into x
-  x.print();
+    x = S.concat(t); // Concatenates the content of t at the end of s and the
+                     // resultant is copied into x
+    x.print();
 
-  S = t; // Copies t in S
+    S = t; // Copies t in S
 
-  S.print();
-  t.print();
+    S.print();
+    t.print();
 }
