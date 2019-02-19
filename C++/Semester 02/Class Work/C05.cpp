893f14f6cf46c14b6227b3afdcfe7b5fd2203ec4
Rafay Ghafoor <rafayghafoor@pm.me>
2019-02-19 12:13:27 +0500

    update

--- C++/Semester 02/Class Work/C05.cpp
+++ C++/Semester 02/Class Work/C05.cpp
@@ -24,9 +24,16 @@ public:
     }
   }
 
-private:
-  int *vec; // Dynamically allocated array
-  int size, cap;
+  // Copy Constructor | Deep Copy
+  Vector(const Vector &obj) {
+    vec = new int[obj.cap];
+
+    for (int i = 0; i < obj.size; i++)
+      vec[i] = obj.vec[i];
+
+    size = obj.size;
+    cap = obj.cap;
+  }
 
   void push_back(int input) {
     if (!cap)
@@ -45,17 +52,60 @@ private:
       vec = temp;
       cap *= 2;
     }
-
     vec[size++] = input;
   }
+
+  void pop_back() {
+    // Removes the last thing
+    // If size has fallen half the capacity, it shrinks the array
+    if (size == cap / 2) {
+      int *temp = new int[cap / 2];
+
+      for (int i = 0; i < cap; i++)
+        temp[i] = vec[i];
+
+      delete[] vec;
+      vec = nullptr;
+      vec = temp;
+      cap /= 2;
+    }
+
+    if (size)
+      size--;
+  }
+
+  void print() {
+    for (int i = 0; i < size; i++) {
+      cout << vec[i] << '\t';
+    }
+  }
+
+  void assign(const Vector &obj) {
+    delete[] vec;
+    vec = nullptr;
+    vec = new int[obj.cap];
+    cap = obj.cap;
+    size = obj.size;
+    for (int i = 0; i < size; i++)
+      vec[i] = obj.vec[i];
+  }
+
+private:
+  int *vec; // Dynamically allocated array
+  int size, cap;
 };
 
 int main() {
-  Vector v1, v2(50), v3; // Empty dynamic array, Spaces allocated for 50 members
-  for (int i = 0; i < 100; i++) {
+  Vector v1, v2(50); // Empty dynamic array, Space allocated for 50 members
+  for (int i = 0; i < 100; i++)
     v2.push_back(i);
-  }
-  Vector v3 = v2; // Copy Constructor i.e., v3(v2)
-  // v3 = v2; Assignment
+
+  v2.print();
+  // Default Copy Constructor / Shallow Copying is called which does member-wise
+  // copy
+  Vector v3 = v2; // Copy Constructor i.e., v3(v2), Memberwise Copy
+
+  //   v3 = v2; Assignment
+  // v1.assign(v2);
   return 0;
 }
