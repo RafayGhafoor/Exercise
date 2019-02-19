452655b69096c325a2dccef65490bbe2c09332f6
Rafay Ghafoor <rafayghafoor@pm.me>
2019-02-19 11:40:48 +0500

    test

--- C++/Semester 02/Class Work/C05.cpp
+++ C++/Semester 02/Class Work/C05.cpp
@@ -3,70 +3,59 @@
 
 using namespace std;
 
-class Vector
-{
-  public:
-    // Default Constructor
-    Vector()
-    {
-        vec = nullptr;
-        size = cap = 0;
+class Vector {
+public:
+  // Default Constructor
+  Vector() {
+    vec = nullptr;
+    size = cap = 0;
+  }
+
+  // Parametric Constructor
+  Vector(int capacity) {
+    vec = new int[capacity];
+    cap = capacity, size = 0;
+  }
+
+  ~Vector() {
+    if (vec) {
+      delete[] vec;
+      vec = nullptr;
+    }
+  }
+
+private:
+  int *vec; // Dynamically allocated array
+  int size, cap;
+
+  void push_back(int input) {
+    if (!cap)
+      vec = new int[++cap];
+
+    else if (size == cap) {
+      // Double the capacity
+      int *temp = new int[cap * 2];
+
+      for (int i = 0; i < cap; i++)
+        temp[i] = vec[i];
+
+      delete[] vec;
+      vec = nullptr;
+
+      vec = temp;
+      cap *= 2;
     }
 
-    // Parametric Constructor
-    Vector(int capacity)
-    {
-        vec = new int[capacity];
-        cap = capacity, size = 0;
-    }
-
-    ~Vector()
-    {
-        if (vec)
-        {
-            delete[] vec;
-            vec = nullptr;
-        }
-    }
-
-  private:
-    int *vec; // Dynamically allocated array
-    int size, cap;
-
-    void push_back(int input)
-    {
-        if (!cap)
-        {
-            vec = new int[++cap];
-        }
-
-        else if (size == cap)
-        {
-            // Double the capacity
-            int *ptr = new int[cap * 2];
-
-            for (int i = 0; i < cap; i++)
-                ptr[i] = vec[i];
-
-            delete[] vec;
-            vec = nullptr;
-
-            vec = ptr;
-            cap *= 2;
-        }
-
-        vec[size++] = input;
-    }
+    vec[size++] = input;
+  }
 };
 
-int main()
-{
-    Vector v1, v2(50), v3; // Empty dynamic array, Spaces allocated for 50 members
-    for (int i = 0; i < 100; i++)
-    {
-        v2.push_back(i);
-    }
-    Vector v3 = v2; // Copy Constructor i.e., v3(v2)
-    // v3 = v2; Assignment
-    return 0;
+int main() {
+  Vector v1, v2(50), v3; // Empty dynamic array, Spaces allocated for 50 members
+  for (int i = 0; i < 100; i++) {
+    v2.push_back(i);
+  }
+  Vector v3 = v2; // Copy Constructor i.e., v3(v2)
+  // v3 = v2; Assignment
+  return 0;
 }
