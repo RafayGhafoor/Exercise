9931070a3a5385e352d2af65bdd17079c0647d04
Rafay Ghafoor <rafayghafoor@pm.me>
2019-03-17 17:40:58 +0500

    update

--- C++/Semester 02/Class Work/C05.cpp
+++ C++/Semester 02/Class Work/C05.cpp
@@ -3,35 +3,29 @@
 
 using namespace std;
 
-class Vector
-{
+class Vector {
 public:
   // Default Constructor
-  Vector()
-  {
+  Vector() {
     vec = nullptr;
     size = cap = 0;
   }
 
   // Parametric Constructor
-  Vector(int capacity)
-  {
+  Vector(int capacity) {
     vec = new int[capacity];
     cap = capacity, size = 0;
   }
 
-  ~Vector()
-  {
-    if (vec)
-    {
+  ~Vector() {
+    if (vec) {
       delete[] vec;
       vec = nullptr;
     }
   }
 
   // Copy Constructor | Deep Copy
-  Vector(const Vector &obj)
-  {
+  Vector(const Vector &obj) {
     vec = new int[obj.cap];
 
     for (int i = 0; i < obj.size; i++)
@@ -41,13 +35,31 @@ public:
     cap = obj.cap;
   }
 
-  void push_back(int input)
-  {
+  int at(int num) {
+    for (int i = 0; i < size; i++) {
+      if (vec[i] == num)
+        return i;
+    }
+    return -1; // if not found
+  }
+
+  int resize(int *&array, const int &size, int extend = 2) {
+    int *resize_arr = new int[size * extend];
+
+    for (int i = 0; i <= size; i++)
+      resize_arr[i] = array[i];
+
+    delete[] array;
+    array = resize_arr;
+
+    return size * 2; // Sends size of the new array
+  }
+
+  void push_back(int input) {
     if (!cap)
       vec = new int[++cap];
 
-    else if (size == cap)
-    {
+    else if (size == cap) {
       // Double the capacity
       int *temp = new int[cap * 2];
 
@@ -63,12 +75,10 @@ public:
     vec[size++] = input;
   }
 
-  void pop_back()
-  {
+  void pop_back() {
     // Removes the last thing
     // If size has fallen half the capacity, it shrinks the array
-    if (size == cap / 2)
-    {
+    if (size == cap / 2) {
       int *temp = new int[cap / 2];
 
       for (int i = 0; i < cap; i++)
@@ -84,16 +94,13 @@ public:
       size--;
   }
 
-  void print()
-  {
-    for (int i = 0; i < size; i++)
-    {
+  void print() {
+    for (int i = 0; i < size; i++) {
       cout << vec[i] << '\t';
     }
   }
 
-  void assign(const Vector &obj)
-  {
+  void assign(const Vector &obj) {
     if (vec)
       delete[] vec;
     vec = new int[obj.cap];
@@ -108,22 +115,21 @@ private:
   int size, cap;
 };
 
-int main()
-{
-  //   Vector v1, v2(50); // Empty dynamic array, Space allocated for 50 members
-  //   for (int i = 0; i < 100; i++)
-  //     v2.push_back(i);
+int main() {
+  Vector v1, v2(50); // Empty dynamic array, Space allocated for 50 members
+  for (int i = 0; i < 100; i++)
+    v2.push_back(i);
 
-  //   v2.print();
+  v2.print();
 
-  // Default Copy Constructor / Shallow Copying is called which does member-wise
-  // copy
-  //   Vector v3 = v2; // Copy Constructor i.e., v3(v2), Memberwise Copy
+  Default Copy Constructor / Shallow Copying is called which does member -
+      wise copy Vector v3 =
+      v2; // Copy Constructor i.e., v3(v2), Memberwise Copy
 
-  //   v3 = v2; Assignment
-  //   v1.assign(v2);
+  v3 = v2;
+  Assignment v1.assign(v2);
 
-  //   v1.print();
+  v1.print();
 
   return 0;
 }
