015dc7e3418715760690140e944f402053da9c5f
RafayGhafoor <rafayghafoor@pm.me>
2019-11-05 14:27:42 +0500

    Update money_prioritizer.cpp

--- C++/Semester 01/money_prioritizer.cpp
+++ C++/Semester 01/money_prioritizer.cpp
@@ -1,20 +1,29 @@
 #include <iostream>
-#include <string>
-#include <stdio.h>
 
 using namespace std;
+
+void splitCurrency(int amount) {
+  int hundreds = amount / 100;
+  amount -= (hundreds * 100);
+  int fifties = amount / 50;
+  amount -= (fifties * 50);
+  int twenties = amount / 20;
+  amount -= (twenties * 20);
+  int tens = (amount / 10);
+  if (hundreds)
+    cout << "\nDenomination for 100s - " << hundreds << endl;
+  if (fifties)
+    cout << "\nDenomination for 50s - " << fifties << endl;
+  if (twenties)
+    cout << "\nDenomination for 20s - " << twenties << endl;
+  if (tens)
+    cout << "\nDenomination for 10s - " << tens << endl << endl;
+}
 
 int main() { 
 	int amount; cout << "Please enter amount: "; cin >> amount;
 	
-	int hundreds = amount / 100;
-	amount -= (hundreds * 100);
-	int fifties = amount / 50;
-	amount -= (fifties * 50);
-	int tens = amount / 10;
-	int ones = amount - (tens * 10);
-	
-	printf("\n100 * %d\n50 * %d\n10 * %d\n1 * %d\n\n", hundreds, fifties, tens, ones);
+	splitCurrency(amount);
 
-	system("pause");
+	
 }
